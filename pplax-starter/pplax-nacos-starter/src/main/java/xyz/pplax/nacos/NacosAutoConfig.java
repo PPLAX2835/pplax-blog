0b1cb3819714313690761462f6242358bd00d3e9
PPLAX2835 <lax1458667357@gmail.com>
2023-09-14 17:14:18 +0800

    pplax-starter:     nacos配置

---
+++ pplax-starter/pplax-nacos-starter/src/main/java/xyz/pplax/nacos/NacosAutoConfig.java
@@ -0,0 +1,13 @@
+package xyz.pplax.nacos;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * nacos配置
+ */
+@EnableDiscoveryClient
+@Configuration
+public class NacosAutoConfig {
+
+}

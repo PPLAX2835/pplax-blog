129db0edf60d7a77214d69af71e77c59a5e3f82f
PPLAX2835 <lax1458667357@gmail.com>
2023-09-15 10:40:51 +0800

    comment-api弄了

---
+++ common-api/comment-api/src/main/java/xyz/pplax/comment/api/service/CommentArticleFeignHandler.java
@@ -0,0 +1,9 @@
+package xyz.pplax.comment.api.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+
+@FeignClient(value = "pplax-article", name = "pplax-article")
+public interface CommentArticleFeignHandler {
+
+}

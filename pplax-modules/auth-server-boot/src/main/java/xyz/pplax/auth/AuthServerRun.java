577fd11554118e2f2d35c5ec70a62cbae82d4a83
PPLAX2835 <lax1458667357@gmail.com>
2023-09-15 15:11:35 +0800

    pplax-modules:     新建auth-server-boot，添加配置文件

---
+++ pplax-modules/auth-server-boot/src/main/java/xyz/pplax/auth/AuthServerRun.java
@@ -0,0 +1,16 @@
+package xyz.pplax.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@EnableAspectJAutoProxy
+@EnableFeignClients
+@SpringBootApplication
+public class AuthServerRun {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerRun.class, args);
+    }
+}

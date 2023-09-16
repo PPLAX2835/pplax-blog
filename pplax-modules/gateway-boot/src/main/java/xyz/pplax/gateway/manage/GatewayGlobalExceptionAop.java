a36ddd06966b9e2720ecaad0d5d42505030216b9
PPLAX2835 <lax1458667357@gmail.com>
2023-09-16 17:39:14 +0800

    gateway-boot:     加上个切面，没写逻辑

---
+++ pplax-modules/gateway-boot/src/main/java/xyz/pplax/gateway/manage/GatewayGlobalExceptionAop.java
@@ -0,0 +1,35 @@
+package xyz.pplax.gateway.manage;
+
+ import lombok.extern.slf4j.Slf4j;
+ import org.aspectj.lang.JoinPoint;
+ import org.aspectj.lang.annotation.AfterThrowing;
+ import org.aspectj.lang.annotation.Aspect;
+ import org.aspectj.lang.annotation.Before;
+ import org.springframework.stereotype.Component;
+ import xyz.pplax.core.util.LogUtils;
+
+
+ @Slf4j
+ @Component
+ @Aspect
+ public class GatewayGlobalExceptionAop {
+
+     public GatewayGlobalExceptionAop() {
+     }
+
+      @AfterThrowing(pointcut = "execution(public * *(..))", throwing = "e")
+      public void getDataFromRedis(Exception e) throws Throwable {
+          LogUtils.logExceptionInfo(e);
+      }
+
+      @AfterThrowing(value = "execution(* *(..))",throwing = "e")
+      public void log(JoinPoint jp, Exception e) {
+          System.out.println("执行异常通知");
+          System.out.println(e.getMessage());
+      }
+
+     @Before("execution(* *(..))")
+     public void beforeAdvice() {
+         System.out.println("Before advice called...");
+     }
+ }

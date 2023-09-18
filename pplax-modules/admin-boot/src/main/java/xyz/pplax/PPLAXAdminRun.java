2ad5ccf19eb3fd63747034f317038ba326268c45
PPLAX2835 <lax1458667357@gmail.com>
2023-09-18 08:34:57 +0800

    新建admin-boot

---
+++ pplax-modules/admin-boot/src/main/java/xyz/pplax/PPLAXAdminRun.java
@@ -0,0 +1,26 @@
+package xyz.pplax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+//import xyz.pplax.admin.service.PermissionRelationService;
+//import xyz.pplax.admin.service.WhiteUrlService;
+//import xyz.pplax.data.entity.Condition;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class PPLAXAdminRun {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(PPLAXAdminRun.class, args);
+
+//        // 将角色权限信息和白名单存入redis
+//        Condition<Long> condition = new Condition<>();
+//        condition.setPageSize(10000);
+//        run.getBean(PermissionRelationService.class).loadAllRolePermission(condition);
+//        run.getBean(WhiteUrlService.class).queryListWhiteUrlByCondition(new Condition<>(){{
+//            setPageSize(10000);
+//        }});
+    }
+}

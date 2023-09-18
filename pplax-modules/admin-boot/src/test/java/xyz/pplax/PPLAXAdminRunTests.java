94eff67c194494a9d19cfb33a281e3e2715e5a45
PPLAX2835 <lax1458667357@gmail.com>
2023-09-18 08:59:36 +0800

    admin-boot:     PermissionAdminRouterRelationshipService

--- pplax-modules/admin-boot/src/test/java/xyz/pplax/PPLAXAdminRunTests.java
+++ pplax-modules/admin-boot/src/test/java/xyz/pplax/PPLAXAdminRunTests.java
@@ -4,7 +4,12 @@ package xyz.pplax;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import xyz.pplax.admin.dto.AdminRouterDTO;
+import xyz.pplax.admin.pojo.RouterPermissionPojo;
 import xyz.pplax.admin.service.AdminRouterService;
+import xyz.pplax.admin.service.PermissionAdminRouterRelationshipService;
+
+import java.util.List;
 
 @SpringBootTest
 public class PPLAXAdminRunTests {
@@ -14,6 +19,18 @@ public class PPLAXAdminRunTests {
     @Test
     public void AdminRouterServiceTest() {
 
+    }
+
+
+    @Autowired
+    PermissionAdminRouterRelationshipService permissionAdminRouterRelationshipService;
+    @Test
+    public void PermissionAdminRouterRelationshipServiceTest() {
+        RouterPermissionPojo routerPermissionPojo = new RouterPermissionPojo();
+        routerPermissionPojo.setUid(12L);
+        routerPermissionPojo.setPermissionUid(12L);
+        List<AdminRouterDTO> adminRouterDTOS = permissionAdminRouterRelationshipService.queryAllAdminRouterInfoByPermissionUid(routerPermissionPojo);
+        System.out.println(adminRouterDTOS);
     }
 
 

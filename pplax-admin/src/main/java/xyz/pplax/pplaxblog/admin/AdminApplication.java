7083a2eed48260fac0a2300002e69cdb9391b898
PPLAX2835 <lax1458667357@gmail.com>
2023-11-26 13:57:25 +0800

    后台初始化

--- pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/AdminApplication.java
+++ pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/AdminApplication.java
@@ -1,5 +1,6 @@
 package xyz.pplax.pplaxblog.admin;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -7,9 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableTransactionManagement
@@ -22,5 +26,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter();
     }
 }
--- pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/restapi/AdminRestApi.java
+++ pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/restapi/AdminRestApi.java
@@ -4,8 +4,10 @@ package xyz.pplax.pplaxblog.admin.restapi;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
 import xyz.pplax.pplaxblog.base.global.SysConf;
 import xyz.pplax.pplaxblog.utils.ResultUtil;
+import xyz.pplax.pplaxblog.utils.StringUtils;
 import xyz.pplax.pplaxblog.xo.entity.Admin;
 import xyz.pplax.pplaxblog.xo.service.AdminService;
 import io.swagger.annotations.Api;
@@ -14,13 +16,12 @@ import io.swagger.annotations.ApiParam;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -31,18 +32,18 @@ import java.util.List;
 @RequestMapping("/admin")
 @Api(value="管理员RestApi",tags={"AdminRestApi"})
 public class AdminRestApi {
-	
+
 	@Autowired
 	AdminService adminService;
-	
+
 	private static Logger log = LogManager.getLogger(AdminRestApi.class);
-	
+
 	@ApiOperation(value="获取管理员列表", notes="获取管理员列表")
 	@GetMapping("/getList")
 	public String getList(HttpServletRequest request,
-			@ApiParam(name = "currentPage", value = "当前页数",required = false) @RequestParam(name = "currentPage", required = false, defaultValue = "1") Long currentPage,
-			@ApiParam(name = "pageSize", value = "每页显示数目",required = false) @RequestParam(name = "pageSize", required = false, defaultValue = "10") Long pageSize) {
-		
+						  @ApiParam(name = "currentPage", value = "当前页数",required = false) @RequestParam(name = "currentPage", required = false, defaultValue = "1") Long currentPage,
+						  @ApiParam(name = "pageSize", value = "每页显示数目",required = false) @RequestParam(name = "pageSize", required = false, defaultValue = "10") Long pageSize) {
+
 		QueryWrapper<Admin> queryWrapper = new QueryWrapper<Admin>();
 		Page<Admin> page = new Page<>();
 		page.setCurrent(currentPage);
@@ -52,16 +53,41 @@ public class AdminRestApi {
 		log.info(list.toString());
 		return ResultUtil.result(SysConf.SUCCESS, list);
 	}
-	
+
 	@ApiOperation(value="用户登录", notes="用户登录")
-	@GetMapping("/login")
-	public String login(HttpServletRequest request, 
-			@ApiParam(name = "username", value = "用户名", required = true) @RequestParam(name = "username", required = true) String username,
-			@ApiParam(name = "password", value = "密码", required = true) @RequestParam(name = "password", required = true) String password) {
+	@PostMapping("/login")
+	public String login(HttpServletRequest request,
+						@ApiParam(name = "username", value = "用户名", required = true) @RequestParam(name = "username", required = true) String username,
+						@ApiParam(name = "password", value = "密码", required = true) @RequestParam(name = "password", required = true) String password) {
+
+		if(StringUtils.isEntity(username) || StringUtils.isEntity(password)) {
+			return ResultUtil.result(SysConf.ERROR, "账号或密码不能为空");
+		}
 		if(username.equals("admin") && password.equals("admin")) {
-			return ResultUtil.result(SysConf.SUCCESS, "success");
+			Map<String, Object> result = new HashMap<>();
+			result.put("token", "admin");
+			return ResultUtil.result(SysConf.SUCCESS, result);
 		}
 		return ResultUtil.result(SysConf.ERROR, "error");
 	}
+
+	@ApiOperation(value = "用户信息", notes = "用户信息", response = String.class)
+	@GetMapping(value = "/info")
+	public String info(@ApiParam(name = "token", value = "token令牌",required = false) @RequestParam(name = "token", required = false) String token) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "admin");
+		map.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+		List<String> list = new ArrayList<String>();
+		list.add("admin");
+		map.put("roles", list);
+		return ResultUtil.result(SysConf.SUCCESS, map);
+	}
+
+	@ApiOperation(value = "退出登录", notes = "退出登录", response = String.class)
+	@PostMapping(value = "/logout")
+	public String logout(@ApiParam(name = "token", value = "token令牌",required = false) @RequestParam(name = "token", required = false) String token) {
+		return ResultUtil.result(SysConf.SUCCESS, null);
+	}
+
 }
 

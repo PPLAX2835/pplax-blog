f12bc62337dbd4f27f660d183fe13436b05491dd
PPLAX2835 <lax1458667357@gmail.com>
2023-11-26 14:08:05 +0800

    后台初始化

--- pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/AdminApplication.java
+++ pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/AdminApplication.java
@@ -1,19 +1,14 @@
 package xyz.pplax.pplaxblog.admin;
 
-import org.apache.catalina.filters.CorsFilter;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableTransactionManagement
@@ -44,6 +39,6 @@ public class AdminApplication {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter();
+        return new CorsFilter(source);
     }
 }
--- pplax-xo/src/main/java/xyz/pplax/pplaxblog/xo/XoApplication.java
+++ pplax-xo/src/main/java/xyz/pplax/pplaxblog/xo/XoApplication.java
@@ -2,8 +2,14 @@ package xyz.pplax.pplaxblog.xo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "xyz.pplax.pplaxblog.xo.controller",
+        "xyz.pplax.pplaxblog.xo.service"})
 public class XoApplication {
     public static void main(String[] args) {
         SpringApplication.run(XoApplication.class, args);

e09b7374bd3c6ed9f8e0420359b2d164f1342ea0
PPLAX2835 <lax1458667357@gmail.com>
2024-01-19 10:15:43 +0800

    整理项目结构

--- pplax-modules/pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/restapi/TestRestApi.java
+++ pplax-modules/pplax-admin/src/main/java/xyz/pplax/pplaxblog/admin/restapi/TestRestApi.java
@@ -2,23 +2,16 @@ package xyz.pplax.pplaxblog.admin.restapi;
 
 
 import com.alibaba.fastjson.JSON;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import xyz.pplax.pplaxblog.base.dto.BaseDto;
 import xyz.pplax.pplaxblog.xo.dto.BlogSortDto;
 import xyz.pplax.pplaxblog.xo.service.BlogService;
 import xyz.pplax.pplaxblog.xo.service.BlogSortService;
 import xyz.pplax.pplaxblog.xo.service.TagService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
--- pplax-modules/pplax-file/src/main/java/xyz/pplax/pplaxblog/file/restapi/StorageRestApi.java
+++ pplax-modules/pplax-file/src/main/java/xyz/pplax/pplaxblog/file/restapi/StorageRestApi.java
@@ -2,14 +2,10 @@ package xyz.pplax.pplaxblog.file.restapi;
 
 
 import io.swagger.annotations.Api;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,9 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "文件存储服务相关接口", tags = {"文件存储服务相关接口"})
 public class StorageRestApi {
     private static final Logger log = LogManager.getLogger(StorageRestApi.class);
-
-    @Value(value = "${file.upload.base-path}")
-    String path;
 
 
 

2539cac9f0629f68d518310f19b67d395e6cf757
PPLAX <lax1458667357@gmail.com>
2024-08-25 16:34:39 +0800

    优化全局拦截器逻辑

--- pplax-modules/pplax-gateway/src/main/java/xyz/pplax/pplaxblog/gateway/filter/RequestLogGlobalFilter.java
+++ pplax-modules/pplax-gateway/src/main/java/xyz/pplax/pplaxblog/gateway/filter/RequestLogGlobalFilter.java
@@ -1,8 +1,8 @@
 package xyz.pplax.pplaxblog.gateway.filter;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import eu.bitwalker.useragentutils.UserAgent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -19,7 +19,6 @@ import reactor.core.publisher.Mono;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import xyz.pplax.pplaxblog.commons.utils.IpUtils;
 import xyz.pplax.pplaxblog.commons.utils.JwtUtil;
-import xyz.pplax.pplaxblog.commons.utils.StringUtils;
 import xyz.pplax.pplaxblog.starter.amqp.constants.MqConstants;
 import xyz.pplax.pplaxblog.xo.constants.type.RequestLogTypeConstants;
 import xyz.pplax.pplaxblog.xo.entity.RequestLog;
@@ -27,12 +26,13 @@ import xyz.pplax.pplaxblog.xo.entity.RequestLog;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 这个组件用于记录每次请求，存储到t_request_log表中
  */
+@Slf4j
 @Component
 public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
 
@@ -41,18 +41,55 @@ public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // 请求开始时间
         long startTime = System.currentTimeMillis();
-
-        // 获取 ServerHttpRequest 对象
         ServerHttpRequest request = exchange.getRequest();
-
-        // 获取请求路径
         String path = request.getURI().getPath();
-
-        // 获取请求方法
         String method = request.getMethodValue();
 
+        RequestLog requestLog = createRequestLog(request, path, method);
+
+        if ("POST".equals(method) && path.startsWith("/api/file")) {
+            return chain.filter(exchange)
+                    .doFinally(signalType -> logRequest(requestLog, startTime, "file params"));
+        }
+
+        return request.getBody().collectList().flatMap(dataBuffers -> {
+            String requestBody = dataBuffers.stream()
+                    .map(this::bufferToString)
+                    .reduce("", String::concat);
+
+            requestLog.setParamsJson(convertRequestParamsToJson(request.getQueryParams(), JSON.parseObject(requestBody)));
+
+            ServerHttpRequestDecorator decoratedRequest = new ServerHttpRequestDecorator(request) {
+                @Override
+                public Flux<DataBuffer> getBody() {
+                    return Flux.just(new DefaultDataBufferFactory().wrap(requestBody.getBytes(StandardCharsets.UTF_8)));
+                }
+            };
+
+            return chain.filter(exchange.mutate().request(decoratedRequest).build())
+                    .doFinally(signalType -> logRequest(requestLog, startTime, requestLog.getParamsJson()));
+        });
+    }
+
+    private void logRequest(RequestLog requestLog, long startTime, String paramsJson) {
+        long duration = System.currentTimeMillis() - startTime;
+        requestLog.setSpendTime(duration);
+        requestLog.setParamsJson(paramsJson);
+        log.info("Request processing time: {} ms", duration);
+        rabbitTemplate.convertAndSend(MqConstants.EXCHANGE_DIRECT, MqConstants.PPLAX_REQUEST_LOG, requestLog);
+    }
+
+    private String bufferToString(DataBuffer buffer) {
+        try {
+            return StreamUtils.copyToString(buffer.asInputStream(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            log.error("Error reading request body", e);
+            return "";
+        }
+    }
+
+    private RequestLog createRequestLog(ServerHttpRequest request, String path, String method) {
         RequestLog requestLog = new RequestLog();
         requestLog.setPath(path);
         requestLog.setMethod(method);
@@ -61,48 +98,8 @@ public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
         requestLog.setAddress(IpUtils.getCityInfo(requestLog.getIp()));
         requestLog.setBrowser(getBrowserInfo(request));
         requestLog.setAccessOs(getOsInfo(request));
-        if (path.contains("/api/admin")) {
-            requestLog.setType(RequestLogTypeConstants.ADMIN);
-        } else {
-            requestLog.setType(RequestLogTypeConstants.WEB);
-        }
-
-        // 提取请求体
-        return request.getBody().collectList().flatMap(dataBuffers -> {
-            String requestBody = dataBuffers.stream()
-                    .map(buffer -> {
-                        try {
-                            return StreamUtils.copyToString(buffer.asInputStream(), StandardCharsets.UTF_8);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    })
-                    .reduce("", String::concat);
-
-            // 获得请求参数
-            requestLog.setParamsJson(convertRequestParamsToJson(request.getQueryParams(), JSON.parseObject(requestBody)));
-
-            // 重新创建一个ServerHttpRequestDecorator，以便能够重新读取请求体
-            ServerHttpRequestDecorator decoratedRequest = new ServerHttpRequestDecorator(request) {
-                @Override
-                public Flux<DataBuffer> getBody() {
-                    return Flux.just(new DefaultDataBufferFactory().wrap(requestBody.getBytes(StandardCharsets.UTF_8)));
-                }
-            };
-
-            // 更新exchange对象中的request
-            return chain.filter(exchange.mutate().request(decoratedRequest).build())
-                    .doFinally(signalType -> {
-                        // 计算请求耗时
-                        long endTime = System.currentTimeMillis();
-                        long duration = endTime - startTime;
-                        requestLog.setSpendTime(duration);
-                        System.out.println("Request processing time: " + duration + " ms");
-                        // 交给消息队列
-                        rabbitTemplate.convertAndSend(MqConstants.EXCHANGE_DIRECT, MqConstants.PPLAX_REQUEST_LOG, requestLog);
-                    });
-        });
+        requestLog.setType(path.startsWith("/api/admin") ? RequestLogTypeConstants.ADMIN : RequestLogTypeConstants.WEB);
+        return requestLog;
     }
 
     @Override
@@ -110,90 +107,40 @@ public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
         return 0; // 过滤器的执行顺序
     }
 
-
-    /**
-     * 获取登录用户uid
-     * @param serverHttpRequest
-     * @return
-     */
     private String getUserUid(ServerHttpRequest serverHttpRequest) {
-        String userUid = null;
-        List<String> authorization = serverHttpRequest.getHeaders().get("Authorization");
-        if (authorization != null && !StringUtils.isEmpty(authorization.get(0))) {
-            String accessToken = authorization.get(0).replace("Bearer ", "");
-            String payloadByBase64 = JwtUtil.getPayloadByBase64(accessToken);
-            if (payloadByBase64 == null) {
-                return null;
-            }
-            JSONObject jsonObject = JSON.parseObject(payloadByBase64);
-            userUid = (String) jsonObject.get("uid");
-        }
-
-        return userUid;
+        return Optional.ofNullable(serverHttpRequest.getHeaders().get("Authorization"))
+                .filter(authHeaders -> !authHeaders.isEmpty())
+                .map(authHeaders -> authHeaders.get(0).replace("Bearer ", ""))
+                .map(JwtUtil::getPayloadByBase64)
+                .map(payload -> JSON.parseObject(payload).getString("uid"))
+                .orElse(null);
     }
 
-    /**
-     * 获取IP地址
-     * @param request
-     * @return
-     */
     public static String getIpAddress(ServerHttpRequest request) {
-        // 获取请求头中的 X-Forwarded-For，用于处理通过代理服务器的请求
-        String ipAddress = request.getHeaders().getFirst("X-Forwarded-For");
-
-        if (ipAddress != null && !ipAddress.isEmpty() && !"unknown".equalsIgnoreCase(ipAddress)) {
-            // X-Forwarded-For 可能包含多个IP地址，取第一个
-            ipAddress = ipAddress.split(",")[0];
-        } else {
-            // 如果没有通过代理，那么直接获取远程地址
-            ipAddress = request.getRemoteAddress().getAddress().getHostAddress();
-        }
-
-        return ipAddress;
+        return Optional.ofNullable(request.getHeaders().getFirst("X-Forwarded-For"))
+                .filter(ip -> !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip))
+                .map(ip -> ip.split(",")[0])
+                .orElseGet(() -> request.getRemoteAddress().getAddress().getHostAddress());
     }
 
-    /**
-     * 获得参数的json串
-     * @param queryParams
-     * @param bodyParams
-     * @return
-     */
     private static String convertRequestParamsToJson(Object queryParams, Object bodyParams) {
-        // 获取所有查询参数
         Map<String, Object> res = new HashMap<>();
         res.put("queryParams", queryParams);
         res.put("bodyParams", bodyParams);
-
-        // 将参数转换为JSON字符串
         return JSON.toJSONString(res);
     }
-    /**
-     * 从ServerHttpRequest对象中获取浏览器信息.
-     *
-     * @param request ServerHttpRequest对象
-     * @return 浏览器名称和版本
-     */
+
     private static String getBrowserInfo(ServerHttpRequest request) {
-        String userAgentString = request.getHeaders().getFirst("User-Agent");
-        if (userAgentString != null) {
-            UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
-            return userAgent.getBrowser().getName() + " " + userAgent.getBrowserVersion();
-        }
-        return "Unknown Browser";
+        return Optional.ofNullable(request.getHeaders().getFirst("User-Agent"))
+                .map(UserAgent::parseUserAgentString)
+                .map(userAgent -> userAgent.getBrowser().getName() + " " + userAgent.getBrowserVersion())
+                .orElse("Unknown Browser");
     }
 
-    /**
-     * 从ServerHttpRequest对象中获取操作系统信息.
-     *
-     * @param request ServerHttpRequest对象
-     * @return 操作系统名称
-     */
     private static String getOsInfo(ServerHttpRequest request) {
-        String userAgentString = request.getHeaders().getFirst("User-Agent");
-        if (userAgentString != null) {
-            UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
-            return userAgent.getOperatingSystem().getName();
-        }
-        return "Unknown OS";
+        return Optional.ofNullable(request.getHeaders().getFirst("User-Agent"))
+                .map(UserAgent::parseUserAgentString)
+                .map(userAgent -> userAgent.getOperatingSystem().getName())
+                .orElse("Unknown OS");
     }
 }
